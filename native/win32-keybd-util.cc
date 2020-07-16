ac31298963e48827b75ca035b1650188dd0342b4
jjkim <objects76@gmail.com>
2020-07-16 17:42:32 +0900

    hook api to hook() function to error handling.

--- native/win32-keybd-util.cc
+++ native/win32-keybd-util.cc
@@ -15,11 +15,7 @@ using namespace std::chrono_literals;
 class LLHook
 {
 public:
-    LLHook(HWND hWnd)
-    {
-        LLHook::hTargetWnd = hWnd;
-        loopFuture = std::async(std::launch::async, &LLHook::messageLoop, this);
-    }
+    LLHook() = default;
     ~LLHook()
     {
         ::PostThreadMessageW(loopThreadId, WM_QUIT, 0, 0);
@@ -28,44 +24,45 @@ public:
             loopFuture.wait_for(5s);
             loopFuture.get();
         }
+        
+        if (hKeyHook)
+            ::UnhookWindowsHookEx(hKeyHook);
     }
     void Pause()
     {
         pause_hook = true;
-        DisableLockWorkstation(false);
     }
     void Resume()
     {
         pause_hook = false;
-        DisableLockWorkstation(true);
     }
 
-    // Windows Registry Editor Version 5.00
-    // [HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Policies\System]
-    // "DisableLockWorkstation"=dword:00000000
+    bool hook(HWND hWnd) 
+    {
+        static auto _this = this;
+        HMODULE hInstance = ::GetModuleHandleA(TOSTR(NODE_GYP_MODULE_NAME) ".node");
+        hKeyHook = ::SetWindowsHookExW(
+            WH_KEYBOARD_LL, [](int nCode, WPARAM wParam, LPARAM lParam) -> LRESULT {
+                return _this->LLKeyboardProc(nCode, wParam, lParam);
+            },
+            hInstance, 0);
+
+        if (hKeyHook) {
+            hTargetWnd = hWnd;
+            loopFuture = std::async(std::launch::async, [this]{
+                loopThreadId = ::GetCurrentThreadId();
+                for (MSG msg; GetMessageW(&msg, nullptr, 0, 0) > 0; );
+                LOG("monitor stopped");
+                return 0;
+            });
+            return true;
+        }
+
+        log("keyhook failed: errno=%d\n", GetLastError());
+        return false;
+    }
 
 private:
-    void DisableLockWorkstation(bool disable)
-    {
-        // STARTUPINFO si = {sizeof(STARTUPINFO)};
-        // si.dwFlags = STARTF_USESHOWWINDOW | STARTF_USESTDHANDLES;
-        // si.wShowWindow = SW_HIDE;
-
-        // char cmdbuf[512];
-        // strcpy_s(cmdbuf, sizeof(cmdbuf), disable ? R"(reg add HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\System /v DisableLockWorkstation /t REG_DWORD /d 1 /f)" : R"(reg delete HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\System /v DisableLockWorkstation /f)");
-
-        // PROCESS_INFORMATION procInfo = {0};
-        // uint32_t createFlags = CREATE_NEW_CONSOLE * 0 | CREATE_NO_WINDOW; // without window.
-        // const char *curDir = nullptr;
-        // if (!CreateProcessA(nullptr, cmdbuf, nullptr, nullptr, TRUE, createFlags, nullptr, curDir, &si, &procInfo))
-        // {
-        //     log("registry op: errno=%d\n", GetLastError());
-        // }
-        // ::WaitForSingleObject(procInfo.hProcess, INFINITE);
-        // ::CloseHandle(procInfo.hThread);
-        // ::CloseHandle(procInfo.hProcess);
-    }
-
     LRESULT CALLBACK LLKeyboardProc(int nCode, WPARAM wParam, LPARAM lParam)
     {
         if (nCode == HC_ACTION && pause_hook == false)
@@ -82,14 +79,11 @@ private:
                 case VK_LMENU: case VK_RMENU:
                 case VK_LCONTROL: case VK_RCONTROL:
                     if (isDev)
-                        log("winkey block %s\n", ((wParam & 1) ? "UP" : "DOWN"));
-                    //if (::GetForegroundWindow() == hTargetWnd)
-                    {
-                        assert(hTargetWnd);
-                        const bool keyup = !!(wParam & 1);
-                        ::PostMessageA(hTargetWnd, wParam, keybdhs->vkCode, keyup ? 0xc15b0001 : 0x415b0001);
-                        return TRUE;
-                    }
+                        log("key.%x block %s\n", keybdhs->vkCode, ((wParam & 1) ? "UP" : "DOWN"));
+                    assert(hTargetWnd);
+                    const bool keyup = !!(wParam & 1);
+                    ::PostMessageA(hTargetWnd, wParam, keybdhs->vkCode, keyup ? 0xc15b0001 : 0x415b0001);
+                    return TRUE;
                 }
             }
         }
@@ -100,36 +94,7 @@ private:
     uint32_t loopThreadId = 0;
     bool pause_hook = false;
     HWND hTargetWnd = nullptr;
-
-    int messageLoop()
-    {
-        static auto _this = this;
-        HMODULE hInstance = ::GetModuleHandleA(TOSTR(NODE_GYP_MODULE_NAME) ".node");
-        auto hookHandle = ::SetWindowsHookExW(
-            WH_KEYBOARD_LL, [](int nCode, WPARAM wParam, LPARAM lParam) -> LRESULT {
-                return _this->LLKeyboardProc(nCode, wParam, lParam);
-            },
-            hInstance, 0);
-        Assert1(hookHandle, format("errno=%d", GetLastError));
-
-        devlog("monitor started: module=%p, hook=%p \n", hInstance, hookHandle);
-        Resume();
-
-        MSG msg;
-        loopThreadId = ::GetCurrentThreadId();
-
-        for (;;)
-        {
-            auto result = GetMessageW(&msg, nullptr, 0, 0);
-            if (result <= 0)
-                break;
-        }
-
-        ::UnhookWindowsHookEx(hookHandle);
-
-        devlog("monitor stopped \n");
-        return 0;
-    }
+    HHOOK hKeyHook = nullptr;
 };
 
 static std::unique_ptr<LLHook> _keybdMonitor;
@@ -142,9 +107,16 @@ bool startKeybdMonitor(int64_t hwndNumber)
         return false;
     }
 
-    _keybdMonitor = std::make_unique<LLHook>(hTargetWnd);
-    devlog("install for hwnd.%p: %d(1:ok, 0:fail)\n", hTargetWnd, !!_keybdMonitor);
-    return !!_keybdMonitor;
+    _keybdMonitor = std::make_unique<LLHook>();
+    if (!_keybdMonitor->hook(hTargetWnd))
+    {
+        throw_error(format("setup failed : errno=%d", GetLastError()), __FUNCTION__);
+        _keybdMonitor.reset();
+        return false;
+    }
+
+    log("setup ok at %s\n", __FUNCTION__);
+    return true;
 }
 
 bool stopKeybdMonitor()
@@ -155,7 +127,7 @@ bool stopKeybdMonitor()
         return true;
     }
 
-    devlog("No keybd monitor object at %s\n", __FUNCTION__);
+    log("No keybd monitor object at %s\n", __FUNCTION__);
     return false;
 }
 
@@ -170,7 +142,7 @@ bool pauseResumeKeybdMonitor(bool resume)
         return true;
     }
 
-    devlog("No keybd monitor object at %s\n", __FUNCTION__);
+    log("No keybd monitor object at %s\n", __FUNCTION__);
     return false;
 }
 
